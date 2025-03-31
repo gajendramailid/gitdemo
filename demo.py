97395779943477e99aee518f89b0b768890d76ca
Gajendra Chauhan <gajendramailid@gmail.com>
2025-03-31 17:14:57 +0530

    Update demo.py to introduce a merge conflict example

--- demo.py
+++ demo.py
@@ -2,12 +2,7 @@
 2
 3
 4
-
-
-
-
-
-
-
+this is 5th line and creating cnflict from vs code
+diff line
 
 # condtion for megre conflict is that the changes should occuer in the same file and same line
