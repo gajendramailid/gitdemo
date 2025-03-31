58fb2a67e6fe42651afd700b32ed4e7f8f233f4e
Gajendra Chauhan <71353121+gajendramailid@users.noreply.github.com>
2025-03-31 14:22:52 +0530

    demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 print("hello inuron world")
 x = 10
 y = 20
-print("ths is athe latertation tot he code")
+print("this os from github")
 print("this is the latest iteration to the code1")
 print("this is the latest 345672")
 print("this is lien 10,  lets us create a cibfluiect to learfn")
